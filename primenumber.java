21f9f59a6f6c2ffe768eb10c33db4c28ecb4dfc2
Akashr24 <akash.23cs008@sode-edu.in>
2024-10-29 21:32:16 +0530

    Add files via upload
    to find prime numbers between two numbers

--- primenumber.java
+++ primenumber.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class primenumber {
 	public static void main(String[] args) {
-		@SuppressWarnings("resource")
 		Scanner sc1 = new Scanner(System.in);
 		int i,j;
+		int flag=0;
 		int k=0;
 		System.out.println("Enter the value1:");
 		int I1=sc1.nextInt();
@@ -15,15 +15,17 @@ public class primenumber {
 		for(i=I1;i<I2;i++) {
 			for(j=2;j<i;j++) {
 				  k = i%j;
-			
-				while(k!=0) {
-					System.out.println(i);
-					break;
+				  if(k==0) {
+						flag=1;
+						break;
+						}
+				  }
+			if(flag==0) {
+				System.out.println(i);
+				}flag=0;
 				}
-			
-			}
-		}
-	}
-}
+		
+	          }
+         }
 
 	
